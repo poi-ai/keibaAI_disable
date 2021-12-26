708a393e5885a5d36476bc4b9409947890ee50e3
POI <67753238+poi-ai@users.noreply.github.com>
2021-12-27 00:33:43 +0900

    枠だけ

---
+++ src/scraping/Netkeiba.py
@@ -0,0 +1,13 @@
+import csv
+import sys
+import time
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, date, timedelta
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
