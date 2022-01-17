d494f175e3b35223e86dc0ac147ed5652dda45ef
POI <jinroup@gmail.com>
2022-01-17 23:25:51 +0900

    documentコメント追加、いらないメソッド削除

--- src/scraping/common/Soup.py
+++ src/scraping/common/Soup.py
@@ -3,10 +3,17 @@ import time
 from bs4 import BeautifulSoup
 
 def get_soup(URL):
+    '''指定したURLからHTMLタグをスクレイピングするメソッド。
+       GETの際にsleepを忘れると大変なことになるので、
+       必ずこのメソッドを呼び出す形でGETを行う。
+
+    Args:
+        URL(str):抽出対象のURL
+    
+    Retuens:
+        soup(bs4.BeautifulSoup):抽出したHTMLタグ
+    
+    '''
     time.sleep(2)
     r = requests.get(URL)
     return BeautifulSoup(r.content, 'lxml')
-
-def del_tag(soup):
-    text = soup.get_text(strip = False)
-    return text
