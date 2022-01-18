58fcdd4406f3a9f91ff2ce75285b312fe06e48e1
POI <jinroup@gmail.com>
2022-01-18 23:07:12 +0900

    システム稼働日のレースID、レース時間を抽出処理追加

--- src/scraping/SurveillanceOdds.py
+++ src/scraping/SurveillanceOdds.py
@@ -4,20 +4,51 @@ from common import Soup
 def main():
     # 日本時間(JST)の環境で実行する場合はこっち
     TODAY = datetime.datetime.today().strftime('%Y%m%d')
-    # herokuなど協定世界時(UTC)の環境で日本時間に合わせる場合は↓を使用
+
+    # herokuなど協定世界時(UTC)の環境で日本時間に合わせる場合はこっち
     # TODAY = (datetime.datetime.now() + datetime.timedelta(hours = 9)).strftime('%Y%m%d')
 
-    get_table(TODAY)
+    TODAY = '20220115'
+    TODAY = '20220122'
 
+    soup = get_soup(TODAY)
+    race_id = []
+    
+    # aタグ取得
+    links = soup.find_all('a')
 
-def get_table(date):
-    URL = 'https://race.netkeiba.com/top/race_list_sub.html?kaisai_date=' + date
-    soup = Soup.get_soup(URL)
-    race_data = soup.find_all('li', class_='RaceList_DataItem')
+    # 空(= 開催なし)の場合 
+    if links == []:
+        exit()
+    
+    # レースURLからIDのみ抽出
+    for link in links:
+        a_url = link.get('href')
+        if 'shutuba.html' in a_url:
+            race_id.append(a_url[29:41])
 
-    # 本日の開催チェック
-    if race_data == []:
-        return 
+    # レース終了フラグ(0:未、1:監視中、-1：済)
+    rec_flag = [0 for _ in range(len(race_id))]
+
+    # TODO フラグ、レース時間から監視するレースを選ぶ 
+
+def get_soup(TODAY):
+    return Soup.get_soup('https://race.netkeiba.com/top/race_list_sub.html?kaisai_date=' + TODAY)
+
+def get_race_time(TODAY):
+    soup = get_soup(TODAY)
+
+    race_time = []
+
+    # レース時間用のクラス名を抽出
+    times = soup.find_all('span', class_='RaceList_Itemtime')
+    
+    # タグと空白を削除
+    for time in times:
+        race_time.append(time.get_text(strip = False)[:5])
+
+    print(race_time)
+    exit()
 
 if __name__ == '__main__':
     main()
