d1a9c7e8ed04b5a63e58d2a65f3fa002602b9f73
poi-ai <67753238+poi-ai@users.noreply.github.com>
2022-01-24 08:27:48 +0900

    コメント追加

--- src/scraping/SurvOdds.py
+++ src/scraping/SurvOdds.py
@@ -7,8 +7,11 @@ def main():
 
     # herokuなど協定世界時(UTC)の環境で日本時間に合わせる場合はこっち
     # TODAY = (datetime.datetime.now() + datetime.timedelta(hours = 9)).strftime('%Y%m%d')
-
+    
+    # HTML取得
     soup = Soup.get_soup('https://race.netkeiba.com/top/race_list_sub.html?kaisai_date=' + TODAY)
+    
+    # TODO list型からDataFrame型へ差し替え
     race_id = []
     
     # aタグ取得
@@ -22,12 +25,14 @@ def main():
     # レースURLからIDのみ抽出
     for link in links:
         a_url = link.get('href')
+        # 出走前のレースは「shutuba.html」、
+        # 出走後のレースは「result.html」がリンクされている
         if 'shutuba.html' in a_url:
             race_id.append(a_url[29:41])
         elif 'result.html' in a_url:
             race_id.append(a_url[28:40])
 
-    # レース終了フラグ(0:未、1:監視中、-1：済)
+    # レース記録フラグ(0:未、1:監視中、-1：済)
     rec_flag = [0 for _ in range(len(race_id))]
 
     while True:
@@ -44,22 +49,30 @@ def main():
 
 
 def target_check(rec_flag, time_schedule, race_id, TODAY):
+    # 現在時刻取得(JST環境)
     NOW = datetime.datetime.now()
 
+    # 現在時刻取得(UTC環境)
+    # NOW = datetime.datetime.now() + datetime.timedelta(hours = 9)
+
+    # レース記録フラグを探索
     for i in range(len(rec_flag)):
+        # 記録前の場合
         if rec_flag[i] == 0:
             print(time_schedule[i])
             race_time = datetime.datetime.strptime(TODAY + time_schedule[i], '%Y%m%d%H:%M')
             remaining_time = (race_time - NOW).seconds
             if remaining_time < -300:
                 rec_flag[i] = -1
-            
-
             exit()
+        # 記録中の場合
         elif rec_flag[i] == 1:
             pass
     # get_odds()
     return rec_flag
+
+def get_odds()
+    pass
 
 def get_race_time(TODAY):
     soup = Soup.get_soup('https://race.netkeiba.com/top/race_list_sub.html?kaisai_date=' + TODAY)
