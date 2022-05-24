f051c5f58a5cb10fc598830f49925c5d489da6d3
POI <jinroup@gmail.com>
2022-05-25 02:11:10 +0900

    #6 稼働日のレース情報取得まで作成

--- src/scraping/NarRecordOdds.py
+++ src/scraping/NarRecordOdds.py
@@ -1,7 +1,5 @@
-import time
-import requests
 import pandas as pd
-from common import Logger, Soup, Jst, WriteSheet, CourseCodeChange
+from common import Jst, Soup, CourseCodeChange
 
 class NarRecordOdds():
     def __init__(self):
@@ -21,13 +19,17 @@ class NarRecordOdds():
         # 競馬場名をkeiba.goのパラメータで使われている競馬場番号へ変換する
         place_nums = [CourseCodeChange.keibago(place_name) for place_name in place_names]
 
+        # 各競馬場ごとにレース数と発走時刻を取得する
         for place_num in place_nums:
-            LIST_URL = 'https://www.keiba.go.jp/KeibaWeb/TodayRaceInfo/RaceList'
-            k_raceDate = f'{jst.year}%2f{jst.month}%2f{jst.day}'
-            soup = Soup.get_soup(f'{LIST_URL}?k_raceDate={k_raceDate}&k_babaCode={place_num}')
-            print(soup)
+            # 日付パラメータ
+            k_raceDate = f'{jst.year()}%2f{jst.month().zfill(2)}%2f{jst.day().zfill(2)}'
+            # レース情報をDataFrame型で取得
+            race_info = pd.read_html(f'https://www.keiba.go.jp/KeibaWeb/TodayRaceInfo/RaceList?k_raceDate={k_raceDate}&k_babaCode={place_num}')[0]
 
+            # TODO 各レース情報をdic型だか自作型で記録
+            exit()
 
+# 実装確認用
 if __name__ == '__main__':
     jst = Jst.Jst()
 
