16857311bea12a7374efcd7cee0d9f4dc33e15c1
POI <67753238+poi-ai@users.noreply.github.com>
2022-05-25 01:39:46 +0900

    日本時間取得クラスのコメントを修正

--- src/scraping/common/Jst.py
+++ src/scraping/common/Jst.py
@@ -1,74 +1,40 @@
 import datetime
 
 class Jst():
-    def now(self):
-        '''
-        Returns:
-            JST(datetime)
+    '''日本時間を取得するクラス'''
 
-        '''
+    def now(self):
+        '''現在の時刻(JST)をdatetime型で返す'''
         return datetime.datetime.utcnow() + datetime.timedelta(hours=9)
 
     def date(self):
-        '''
-        Returns:
-            JST(str)[YYYYmmdd]
-
-        '''
+        '''現在の日付(JST)をstr型[YYYYmmdd]で返す'''
         return self.now().strftime("%Y%m%d")
 
     def time(self):
-        '''
-        Returns:
-            JST(str)[YYYYmmddHHMMSS]
-
-        '''
+        '''現在の時刻(JST)をstr型[YYYYmmddHHMMSS]で返す'''
         return self.now().strftime("%Y%m%d%H%M%S")
 
     def year(self):
-        '''
-        Returns:
-            JSTの年(int)
-
-        '''
+        '''現在の年(JST)をstr型[YYYY]で返す'''
         return self.now().year
 
     def month(self):
-        '''
-        Returns:
-            JSTの月(int)
-
-        '''
+        '''現在の月(JST)をstr型[m(0埋めなし)]で返す'''
         return self.now().month
 
     def day(self):
-        '''
-        Returns:
-            JSTの日(int)
-
-        '''
+        '''現在の日(JST)をstr型[d(0埋めなし)]で返す'''
         return self.now().day
 
     def hour(self):
-        '''
-        Returns:
-            JSTの時間(int)
-
-        '''
+        '''現在の時間(JST)をstr型[H(0埋めなし)]で返す'''
         return self.now().hour
 
     def minute(self):
-        '''
-        Returns:
-            JSTの分(int)
-
-        '''
+        '''現在の分(JST)をstr型[M(0埋めなし)]で返す'''
         return self.now().minute
 
     def second(self):
-        '''
-        Returns:
-            JSTの秒(int)
-
-        '''
+        '''現在の秒(JST)をstr型[S(0埋めなし)]で返す'''
         return self.now().second
