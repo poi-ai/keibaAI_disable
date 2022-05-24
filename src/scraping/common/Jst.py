1abc47de86dd340c8d4ba12d87a5cf9b0f4639a8
POI <67753238+poi-ai@users.noreply.github.com>
2022-05-25 02:11:10 +0900

    #6 稼働日のレース情報取得まで作成

--- src/scraping/common/Jst.py
+++ src/scraping/common/Jst.py
@@ -17,24 +17,24 @@ class Jst():
 
     def year(self):
         '''現在の年(JST)をstr型[YYYY]で返す'''
-        return self.now().year
+        return str(self.now().year)
 
     def month(self):
         '''現在の月(JST)をstr型[m(0埋めなし)]で返す'''
-        return self.now().month
+        return str(self.now().month)
 
     def day(self):
         '''現在の日(JST)をstr型[d(0埋めなし)]で返す'''
-        return self.now().day
+        return str(self.now().day)
 
     def hour(self):
         '''現在の時間(JST)をstr型[H(0埋めなし)]で返す'''
-        return self.now().hour
+        return str(self.now().hour)
 
     def minute(self):
         '''現在の分(JST)をstr型[M(0埋めなし)]で返す'''
-        return self.now().minute
+        return str(self.now().minute)
 
     def second(self):
         '''現在の秒(JST)をstr型[S(0埋めなし)]で返す'''
-        return self.now().second
+        return str(self.now().second)
