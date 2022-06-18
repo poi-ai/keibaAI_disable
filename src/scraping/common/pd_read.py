11f2d46743cb7910d6f6020fd24e452f97f31a06
POI <jinroup@gmail.com>
2022-06-19 03:06:44 +0900

    #6 エラー時のリトライ処理/ログ出力処理追加

---
+++ src/scraping/common/pd_read.py
@@ -0,0 +1,29 @@
+import logger
+import pandas as pd
+import time
+import traceback
+
+def html(URL, sleep_time = 0, retry_count = 3):
+    '''テーブル取得のリトライ処理
+
+         Args:
+             URL(str):取得対象のURL
+             sleep_time(int):リトライまでのアイドルタイム
+             retry_count(int):最大リトライ回数
+
+         Returns:
+             table(pandas.DataFrame):取得したHTML情報
+    '''
+
+    for _ in range(retry_count):
+        try:
+            table = pd.read_html(URL)
+        except Exception as e:
+            logger.error(e)
+            logger.error(traceback.format_exc())
+            if sleep_time != 0:
+                time.sleep(sleep_time)
+        else:
+            return table
+    else:
+        return -1
